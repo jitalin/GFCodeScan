057a426bffec3dd2b8987ab4f1a35444fba82ce0
daniel <49123890@qq.com>
2016-12-06 10:13:49 +0800

    Add files via upload

---
+++ GFCodeScan/GFCodeScan/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GFCodeScan
+//
+//  Created by 高飞 on 16/12/6.
+//  Copyright © 2016年 高飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
